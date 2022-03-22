0b54e4b27203cc38d9398ac771b70e6f4aed9538
mdumar112233 <mdumarfaruk187326@gmail.com>
2022-03-22 12:04:33 +0600

    concrete api view

--- concrete_api_view/urls.py
+++ concrete_api_view/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('studentconcreteapi/', views.StudentList.as_view()),
-]
+    # path('studentconcreteapi/', views.StudentList.as_view()),
+    # path('studentconcreteapi/', views.CreateAPIView.as_view()),
+    # path('studentconcreteapi/<int:pk>/', views.StudentRetrive.as_view()),
+
+
+    path('studentconcreteapi/', views.StudentListCreate.as_view()),
+
+    path('studentconcreteapi/<int:pk>/', views.StudentRetriveUpdateDestroy.as_view()),
+
+] 
 
 
 
--- concrete_api_view/views.py
+++ concrete_api_view/views.py
@@ -1,11 +1,46 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView,RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import StudentConcreteModel
 from .serializers import StudentSerializer
 
 # Create your views here.
 
-class StudentList(ListAPIView):
+# class StudentList(ListAPIView):
+#     queryset = StudentConcreteModel.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentCreate(CreateAPIView):
+#     queryset = StudentConcreteModel.objects.all()
+#     serializer_class = StudentSerializer
+
+
+# class StudentRetrive(RetrieveAPIView):
+#     queryset = StudentConcreteModel.objects.all()
+#     serializer_class = StudentSerializer
+
+
+##### CUMBINE API VIEW
+
+class StudentListCreate(ListCreateAPIView):
     queryset = StudentConcreteModel.objects.all()
     serializer_class = StudentSerializer
+
+
+class StudentRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = StudentConcreteModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
